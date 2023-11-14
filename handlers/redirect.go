24efff62e97a8c460bfdae2cc7303a4169eb6eca
Xx_DrkLeo_xX <leo.delanoe@etudiant.univ-rennes1.fr>
2023-11-15 00:40:33 +0100

    add the connexion with the db

--- handlers/redirect.go
+++ handlers/redirect.go
@@ -1,3 +1,10 @@
+package handlers
+
+import (
+  "fmt"
+  "net/http"
+)
+
 func Redirect(w http.ResponseWriter, r *http.Request) {
   fmt.Fprintf(w, "Hello, World!\n")
   fmt.Fprintf(w, "%s", r.URL.Path)
--- main.go
+++ main.go
@@ -1,27 +1,34 @@
 package main
 
 import (
- //  "errors"
+	//  "errors"
+	"court/handlers"
 	"fmt"
+	"log"
 	"net/http"
 	"os"
-  "database/sql"
-
-  _ "github.com/lib/pq"
 )
+
 func main() {
 
-  port := os.Getenv("PORT")
-  if port == "" {
-    port = ":8080"
-  } else {
-    port = ":" + port
-  }
+	servPort := GetArg("PORT", "8080")
+	db := OpenDB()
+	defer db.Close()
+	rows, err := db.Query("SELECT * FROM INFORMATION_SCHEMA.TABLES WHERE TABLE_SCHEMA = 'dbo'")
+	defer rows.Close()
+	if err != nil {
+		log.Fatalln(err)
+	}
+	var res string
+	for rows.Next() {
+		rows.Scan(&res)
+		fmt.Println(res)
+	}
 
-  http.HandleFunc("/", Redirect)
-  err := http.ListenAndServe(port, nil)
-  if err != nil {
-    fmt.Printf("Error: %s\n", err)
-    os.Exit(1)
-  }
+	http.HandleFunc("/", handlers.Redirect)
+	err = http.ListenAndServe(":"+servPort, nil)
+	if err != nil {
+		fmt.Printf("Error: %s\n", err)
+		os.Exit(1)
+	}
 }
